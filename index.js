c7f0d268b00c315311b91c7b40b5e9f8c37df877
Denchik136 <pdv@sinergium.ru>
2024-02-13 20:11:42 +0300

    Make product page

--- index.js
+++ index.js
@@ -36,15 +36,18 @@ const replaceTemplate = (temp, product) => {
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8")
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8")
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8")
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url
+
+
+    const { query, pathname } = url.parse(req.url, true);
 
     //Overview page
 
-    if (pathName === "/" || pathName === "/overview") {
+    if (pathname === "/" || pathname === "/overview") {
         res.writeHead(200, { "Content-type": 'text/html' })
 
         const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
@@ -53,11 +56,14 @@ const server = http.createServer((req, res) => {
         res.end(output)
 
         //Product page
-    } else if (pathName === "/product") {
-        res.end("This is the product")
+    } else if (pathname === "/product") {
+        res.writeHead(200, { "Content-type": 'text/html' })
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output)
 
         //API
-    } else if (pathName === "/api") {
+    } else if (pathname === "/api") {
         res.writeHead(200, { "Content-type": "application/json" })
         res.end(data)
 
