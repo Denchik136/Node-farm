9e63947e6eaa2530d12bff8d3e8414fb93fb8218
Denchik136 <pdv@sinergium.ru>
2024-02-20 21:34:46 +0300

    save

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const fs = require("fs")
 const http = require("http")
 const url = require("url")
 
+const express = require('express')
+
+const replaceTemplate = require('./modules/replaceTemplate')
+
+
+
+
 // FILES
 //blocking
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
@@ -18,20 +25,6 @@ const url = require("url")
 // console.log("will read file")
 
 //SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8")
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8")
@@ -40,8 +33,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
-const server = http.createServer((req, res) => {
 
+const server = http.createServer((req, res) => {
 
     const { query, pathname } = url.parse(req.url, true);
 
